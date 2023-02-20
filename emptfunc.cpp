69eea3d92b6ba4a1b7fcea3cdd0152a80015faac
AllenP3 <72021357+AllenP3@users.noreply.github.com>
2023-02-20 13:27:39 +0100

    newday

--- emptfunc.cpp
+++ emptfunc.cpp
@@ -3,16 +3,13 @@ using namespace std;
 
 int y = 65;
 void Increase(){
-    y = y + 1;
+    y + 1;
 }
 
 int main(){
     cout << y <<endl;
-    Increase;
-    Increase;
-    Increase;
-    Increase;
-    Increase;
+    
+    Increase();
 
     cout << y;
 }
---
+++ gradebook.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class GradeBook {
+    public:
+    void set_course_name(string name);
+    string course_name() const;
+    void DisplayMessage() const;
+
+    private:
+    string course_name_;
+};
+
+void GradeBook::set_course_name(string name){
+    course_name_ = name;
+}
+
+string GradeBook::course_name() const {
+    return course_name_;
+}
+
+void GradeBook::DisplayMessage() const {
+    cout <<"Grade book for " << course_name() << endl;
+}
+
+
+int main(){
+
+    string course_name;
+    GradeBook my_grade_book;
+
+    cout << "Initital course name: "<<my_grade_book.course_name();
+
+    cout << "\nPlease enter the course name: ";
+    getline(cin, course_name);
+    my_grade_book.DisplayMessage();
+    
+
+}
